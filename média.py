37cc055cb878c0edd121c641c954a9e97368dfe0
Pedro Augusto Cabral Oliveira <104571614+PedroAugusto2004@users.noreply.github.com>
2023-03-20 15:35:22 -0300

    Create média.py

---
+++ média.py
@@ -0,0 +1,13 @@
+Nota1= int(input("Digite aqui a nota do 1º aluno:"))
+Nota2= int(input("Digite aqui a nota do 2º aluno:"))
+
+
+if Nota1 >= 6:
+    print("Você foi Aprovado!")
+else:
+    print("Sinto muito mas você esta Reprovado")
+
+if Nota2 >= 6:
+    print("Você foi Aprovado!")
+else:
+    print("Sinto muito mas você esta Reprovado")
